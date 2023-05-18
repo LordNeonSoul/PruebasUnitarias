1885e424f2f17f07288cd15520378d3aa9d9395c
Neón <juan.pablo.pantoja@correounivalle.edu.co>
2023-05-18 16:31:03 -0500

    cambios 13

--- sum.test.js
+++ sum.test.js
@@ -1,14 +1,5 @@
-const istanbul = require('istanbul');
-const collector = new istanbul.Collector();
-const reporter = new istanbul.Reporter();
 const sum = require('./sum');
 
 test('sums two numbers', () => {
   expect(sum(1, 2)).toBe(3);
 });
-
-collector.add(global['__coverage__']);
-
-// Genera los informes de cobertura
-reporter.addAll(['text-summary', 'lcov']);
-reporter.write(collector, true, () => {});
